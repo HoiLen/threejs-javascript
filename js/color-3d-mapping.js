a0f91c25481b3b4bb6699f295e45946e9781a15b
HoiLen <len1231.hoi@gmail.com>
2022-11-07 16:24:22 +0900

    create color-3d-mapping

---
+++ js/color-3d-mapping.js
@@ -0,0 +1,97 @@
+import * as THREE from 'three';
+// import { GUI } from '../node_modules/three/examples/jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { TransformControls } from '../node_modules/three/examples/jsm/controls/TransformControls.js';
+
+//htmlからcontainer要素を取り出す。
+const container = document.getElementById('container');
+
+
+/*--------------------3Dシーンの設定--------------------*/
+
+
+//シーン
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+//カメラ
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.set(0, 500, 1000);
+scene.add(camera);
+//環境光
+scene.add(new THREE.AmbientLight(0xf0f0f0));
+const light = new THREE.SpotLight(0xffffff);
+light.position.set(0, 1500, 200);
+light.angle = Math.PI * 0.2;
+light.castShadow = true;
+light.shadow.camera.near = 200;
+light.shadow.camera.far = 2000;
+light.shadow.bias = - 0.000222;
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+light.intensty = 200;
+scene.add(light);
+//レンダラー
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+container.appendChild(renderer.domElement);//div id=containerの中にcanvasを作成
+
+//視点変更の設定
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.5;
+controls.addEventListener('change', render);//カメラの位置情報が更新されるたびにrender()を呼び出す
+
+//オブジェクトの変形に関する設定
+const transformControl = new TransformControls(camera, renderer.domElement);
+transformControl.addEventListener('change', render);
+//マウスのドラッグで変更されたときに呼び出す
+transformControl.addEventListener('dragging-changed', function (event) {
+    //オブジェクトのトランスフォーム中は、カメラの視点を切り替えられないようにする
+    controls.enabled = !event.value;
+});
+scene.add(transformControl);
+window.addEventListener('resize', onWindowResize);
+
+//x-y平面のグリットの設定
+const helper = new THREE.GridHelper(2000, 100);
+helper.position.y = - 199;
+helper.material.opacity = 0.25;
+helper.material.transparent = true;
+scene.add(helper);
+
+
+/*--------------------処理を書く--------------------*/
+
+init();
+
+
+
+render();
+
+/*--------------------関数--------------------*/
+
+function init() {
+    /*----------初期化処理----------*/
+    console.log("init");
+}
+
+//レンダリング時に実行したい処理をここに書く
+function render() {
+
+    renderer.render(scene, camera);
+
+}
+
+//ウィンドウのサイズにシーンを合わせる
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    controls.update();
+    render();
+}
