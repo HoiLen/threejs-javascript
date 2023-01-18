6852224095553af43edd60640d2d8e088e22b04a
HoiLen <len1231.hoi@gmail.com>
2023-01-18 14:35:11 +0900

    すすめた。

--- js/POASON_TEST2.js
+++ js/POASON_TEST2.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { GUI } from '../node_modules/three/examples/jsm/libs/lil-gui.module.min.js';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { TransformControls } from '../node_modules/three/examples/jsm/controls/TransformControls.js';
+//import { TransformControls } from '../node_modules/three/examples/jsm/controls/TransformControls.js';
 
 
 
@@ -49,15 +49,15 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.5;
 controls.addEventListener('change', render);//カメラの位置情報が更新されるたびにrender()を呼び出す
 
-//オブジェクトの変形に関する設定
-const transformControl = new TransformControls(camera, renderer.domElement);
-transformControl.addEventListener('change', render);
-//マウスのドラッグで変更されたときに呼び出す
-transformControl.addEventListener('dragging-changed', function (event) {
-    //オブジェクトのトランスフォーム中は、カメラの視点を切り替えられないようにする
-    controls.enabled = !event.value;
-});
-scene.add(transformControl);
+// //オブジェクトの変形に関する設定
+// const transformControl = new TransformControls(camera, renderer.domElement);
+// transformControl.addEventListener('change', render);
+// //マウスのドラッグで変更されたときに呼び出す
+// transformControl.addEventListener('dragging-changed', function (event) {
+//     //オブジェクトのトランスフォーム中は、カメラの視点を切り替えられないようにする
+//     controls.enabled = !event.value;
+// });
+// scene.add(transformControl);
 window.addEventListener('resize', onWindowResize);
 
 //x-y平面のグリットの設定
@@ -99,9 +99,20 @@ const gui = new GUI();
 gui.add(params, 'gateValue', 0, 1).step(0.05).onChange(function (value) {
 
     Gslider = value;
-    data[0] = {x:'0', red:20+Gslider*10};
-    myChart.update();
-    //myChart.update();
+
+    data[1].red = 28 + Gslider * 15;
+    data[2].red = 33 + Gslider * 15;
+    data[3].red = 34 + Gslider * 15;
+    data[4].red = 34 + Gslider * 15;
+    data[5].red = 34 + Gslider * 15;
+    data[6].red = 34 + Gslider * 15;
+
+    dataBVG[0].x = Gslider * 6;
+    dataBVG[1].x = Gslider * 6;
+
+    idvdChart.update();
+    idvgChart.update();
+
     vertices.length = 0; //点群を初期化
     addPoints();
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
@@ -111,7 +122,19 @@ gui.add(params, 'gateValue', 0, 1).step(0.05).onChange(function (value) {
 gui.add(params, 'drainValue', 0, 1).step(0.05).onChange(function (value) {
 
     Dslider = value;
-    //myChart.update();
+
+    datavg[3].red = 18 + Dslider * 8;
+    datavg[4].red = 32 + Dslider * 20;
+    datavg[5].red = 56 + Dslider * 30;
+    datavg[6].red = 80 + Dslider * 70;
+
+    dataBVD[0].x = Dslider * 6;
+    dataBVD[1].x = Dslider * 6;
+
+
+    idvgChart.update();
+    idvdChart.update();
+
     vertices.length = 0; //点群を初期化
     addPoints();
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
@@ -228,8 +251,8 @@ function addPoints() {
 
 //レンダリング時に実行したい処理をここに書く
 function render() {
-    //myChart.update();
-    
+    //idvdChart.update();
+
     mesh.geometry.attributes.position.needsUpdate = true;
     renderer.render(scene, camera);
 
@@ -258,7 +281,7 @@ const promise = new Promise((resolve, reject) => {
     console.log(vertices);
     setTimeout(() => {
         resolve();
-    }, 1500);
+    }, 500);
 
 })
     .then(() => {
@@ -301,43 +324,178 @@ const promise = new Promise((resolve, reject) => {
 /*------------------------------------ここからchart.js------------------------------------*/
 
 var chartCanvas;
+var idvgCanvas;
 
 chartCanvas = document.createElement("canvas");
 chartCanvas.id = 'chartcanvas';
 
+idvgCanvas = document.createElement("canvas");
+idvgCanvas.id = 'idvgcanvas';
+
 var cbox = document.getElementById('container');
 cbox.appendChild(chartCanvas);
+//var idvgbox = document.getElementById('container');
+cbox.appendChild(idvgCanvas);
 
 var ctx = document.querySelector("#chartcanvas");
-ctx.width = 16;
-ctx.height = 9;
+var vgx = document.querySelector("#idvgcanvas");
+ctx.width = 1;
+ctx.height = 1;
+vgx.width = 1;
+vgx.height = 1
 
-var testn = 9;
 
+// var data = [
+//     { x: '0', red: 20 },
+//     { x: 'Th', red: 28 },
+//     { x: 'Over', red: 33 },
+//     { x: ' ', red: 34 },
+//     { x: '  ', red: 34 },
+//     { x: '   ', red: 34 },
+//     { x: 'Dorain Vol', red: 34 },
+// ];
 var data = [
-    { x: '0', red: 20 },
-    { x: 'Th', red: 35 },
-    { x: 'Over', red: 40 },
-    { x: ' ', red: 44 },
-    { x: '  ', red: 47 },
-    { x: '   ', red: 49 },
-    { x: 'Dorain Voltage', red: 50 },
+    { x: 0, red: 20 },
+    { x: 1, red: 28 },
+    { x: 2, red: 33 },
+    { x: 3, red: 34 },
+    { x: 4, red: 34 },
+    { x: 5, red: 34 },
+    { x: 6, red: 34 },
 ];
-var myChart = new Chart(ctx, {
-    type: 'line',
+var datavg = [
+    { x: 0, red: 0 },
+    { x: 1, red: 4 },
+    { x: 2, red: 10 },
+    { x: 3, red: 18 },
+    { x: 4, red: 32 },
+    { x: 5, red: 56 },
+    { x: 6, red: 80 },
+];
+var dataBVD = [
+    { x: 0, red: 0 },
+    { x: 0, red: 100 },
+];
+var dataBVG = [
+    { x: 0, red: 0 },
+    { x: 0, red: 100 },
+];
+
+
+var idvdChart = new Chart(ctx, {
+    // type: 'line',
+    type: 'scatter',
     data: {
-        //labels: ['0', 'Th', 'Over'],
-        datasets: [{
-            label: 'Red',
-            borderColor: '#f00',
-            data: data,
-            parsing: { yAxisKey: 'red' },
-        }
+        //labels: [0, 1, 2, 3, 4, 5, 6],
+        datasets: [
+            {
+                label: 'Red',
+                borderColor: '#aaa',
+
+                data: data,
+                parsing: { yAxisKey: 'red' },
+                showLine: true,
+                tension: 0.3,
+                pointRadius: 0,
+            },
+            {
+                label: 'blue',
+                borderColor: '#0f0',
+                data: dataBVD,
+                parsing: { yAxisKey: 'red' },
+                showLine: true,
+                tension: 0.3,
+                pointRadius: 0,
+            }
+        ],
+    },
+    options: {
+
+        animation: false,
+        maintainAspectRatio: true,
+        scales: {
+            x: {
+                min: 0,
+                max: 6,
+                ticks: {
+                    stepSize: 0.05 * 6,
+                    // maxTicksLimit: 100,
+                },
+            },
+            y: {
+                max: 60,
+                min: 20,
+                ticks: {
+                    stepSize: 5,
+                    display: false,
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
+    },
+});
+ctx.parentNode.style.width = '20%';
+
+var idvgChart = new Chart(vgx, {
+    type: 'scatter',
+    data: {
+        //labels: [0, 1, 2, 3, 4, 5, 6],
+        datasets: [
+            {
+                label: 'Red',
+                borderColor: '#aaa',
+                data: datavg,
+                parsing: { yAxisKey: 'red' },
+                showLine: true,
+                tension: 0.4,
+                pointRadius: 0,
+            },
+            {
+                label: 'blue',
+                borderColor: '#0f0',
+                data: dataBVG,
+                parsing: { yAxisKey: 'red' },
+                showLine: true,
+                tension: 0.3,
+                pointRadius: 0,
+            }
         ],
     },
     options: {
         animation: false,
+        maintainAspectRatio: true,
+        scales: {
+            x: {
+                min: 0,
+                max: 6,
+                ticks: {
+                    stepSize: 0.05 * 6,
+                    // maxTicksLimit: 100,
+                },
+            },
+            y: {
+                max: 80,
+                min: 0,
+                ticks: {
+                    stepSize: 5,
+                    display: false,
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
     },
 });
-ctx.parentNode.style.width = '500px';
-
+//ctx.parentNode.style.width = '20%';
+//ctx.parentNode.style.height = '40vh';
+//vgx.parentNode.style.width = '20%';
+vgx.parentNode.style.width = '20%';
+//vgx.parentNode.style.height = '40vh';
+//vgx.parentNode.style.height = '40vh';
